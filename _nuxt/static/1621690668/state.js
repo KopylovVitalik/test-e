c537b578ed6f5a34e7370cf2c642e5d766c5436d
KopylovVitalii <vitalik.kopylov@gmail.com>
2021-05-22 16:38:24 +0300

    a56cd4c

---
+++ _nuxt/static/1621690668/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftest-e\u002F_nuxt\u002Fstatic\u002F1621690668",layout:"default",error:a,state:{favoritesListIds:[],modalOpen:false},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ftest-e\u002F",assetsPath:"\u002Ftest-e\u002F_nuxt\u002F",cdnURL:a}}}}(null));
